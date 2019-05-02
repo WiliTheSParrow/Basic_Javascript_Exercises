b2a283b098748d00c861ac4f2c6458caa249ab97
WiliTheSParrow <w.julia91@gmail.com>
2019-05-02 10:49:04 +0200

    continue, 5th

--- programmingfundaments.js
+++ programmingfundaments.js
@@ -1,7 +1,7 @@
 // PROGRAMMING FUNDAMENTS IN JAVASCRIPT
 
 // 1. addition:
-var exampleArray = [1, 1, 3, 6, 33, 100];
+var exampleArray = [1, 1, 3, 6, 33, 100, 2];
 var additionResult = 0;
 
 for (i = 0; i < exampleArray.length; i++) {
@@ -13,7 +13,7 @@ console.log(additionResult);
 // 2. numerable:
 
 var numerableResult = 0;
-var examineNo = 33;
+var examineNo = 2;
 
 for (i = 0; i < exampleArray.length; i++) {
     if (exampleArray[i] > examineNo) {
@@ -32,16 +32,36 @@ while (index01 < exampleArray.length && exampleArray[index01] != examineNo) {
 };
 
 // OR exampleArray[index01] == examineNo in the if
-if (index01<exampleArray.length){
+if (index01 < exampleArray.length) {
     console.log("There is " + examineNo + " in the array!");
 } else {
     console.log("There is NO " + examineNo + " in the array!");
 };
 
 
-// 4.
+// 4. selection:
+var index02 = 0;
 
-// 5.
+while (index02 < exampleArray.length && exampleArray[index02] != examineNo) {
+    index02++;
+};
+
+var resultSelection = index02 + 1;
+
+console.log(resultSelection);
+
+// 5. search:
+var index03 = 0;
+
+while (index03 < exampleArray.length && exampleArray[index03] != examineNo) {
+    index03++;
+};
+
+if(index03<exampleArray.length && exampleArray[index03] == examineNo){
+    console.log("There is number " + examineNo + " on the " + (index03+1) + ". place in the array!")
+} else{
+    console.log("There is no " + examineNo + " number in the array.")
+}
 
 // 6.
 
